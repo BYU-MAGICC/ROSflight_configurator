aa327d4b61dc80ee00912b5c90a68555e940b993
James Jackson <superjax08@gmail.com>
2017-05-01 21:12:11 -0600

    using rickshaw to get IMU streaming

--- chart.js
+++ chart.js
@@ -17,23 +17,8 @@ ros.on('close', function() {
   console.log('Connection to websocket server closed.');
 });
 
-
-// Data for Plots
-var acc_x = {label: 'acc_x', values: []}
-    // acc_y = {label: 'cos', values: []},
-    // acc_z = {label: 'cos', values: []}
-var last_time = 0
-
-var chart = $('#lineChart').epoch({
-  type: 'time.line',
-  data: [acc_x],
-  historySize: 20,
-  queueSize: 1,
-  axes: ['right', 'bottom', 'left'] 
-});
-
-
-// Loading data for plotting
+// Subscribe to and handle ROS messages
+var acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z;
 window.onload = function () {
   var imu_listener = new ROSLIB.Topic({
     ros : ros,
@@ -42,11 +27,42 @@ window.onload = function () {
   });
 
   imu_listener.subscribe(function(msg) {
-    var now = msg.header.stamp.secs + msg.header.stamp.nsecs*1.0e-9;
-    if (now > last_time + 0.01)
-    {
-      chart.push([{time: now, y: msg.linear_acceleration.x}]);
-      last_time = now
-    }
+    acc_x = msg.linear_acceleration.x;
+    acc_y = msg.linear_acceleration.y;
+    acc_z = msg.linear_acceleration.z;
+    gyro_x = msg.linear_acceleration.x;
+    gyro_y = msg.linear_acceleration.y;
+    gyro_z = msg.linear_acceleration.z;
   });
 }
+
+var tv = 30;
+
+// instantiate our graph!
+var graph = new Rickshaw.Graph( {
+  element: document.getElementById("chart"),
+  width: 900,
+  height: 500,
+  renderer: 'line',
+  series: new Rickshaw.Series.FixedDuration([{ name: 'acc_x' }, { name: 'acc_y' }, { name: 'acc_z' }], undefined, {
+    timeInterval: tv,
+    maxDataPoints: 100, 
+    timeBase: new Date().getTime() / 1000
+  }) 
+} );
+
+graph.render();
+
+// add some data every so often
+
+var i = 0;
+var iv = setInterval( function() {
+
+  var data = { one: acc_x };
+  data.two = acc_y
+  data.three = acc_z;
+
+  graph.series.addData(data);
+  graph.render();
+
+}, tv );
