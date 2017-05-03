71c360e660976a3fc879740f32eb72baa2128704
James Jackson <superjax08@gmail.com>
2017-05-02 21:34:35 -0600

    finished accel, gyro, magnetometer and barometer plotting, I hate CSS

--- chart.js
+++ chart.js
@@ -18,7 +18,8 @@ ros.on('close', function() {
 });
 
 // Subscribe to and handle ROS messages
-var acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z;
+var acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, baro;
+var mag_x, mag_y, mag_z;
 window.onload = function() {
   var imu_listener = new ROSLIB.Topic({
     ros: ros,
@@ -30,19 +31,46 @@ window.onload = function() {
     acc_x = msg.linear_acceleration.x;
     acc_y = msg.linear_acceleration.y;
     acc_z = msg.linear_acceleration.z;
-    gyro_x = msg.linear_acceleration.x;
-    gyro_y = msg.linear_acceleration.y;
-    gyro_z = msg.linear_acceleration.z;
+    gyro_x = msg.angular_velocity.x;
+    gyro_y = msg.angular_velocity.y;
+    gyro_z = msg.angular_velocity.z;
+  });
+
+  var baro_listener = new ROSLIB.Topic({
+    ros: ros,
+    name: '/baro',
+    messageType: 'fcu_common/Barometer'
+  });
+
+  baro_listener.subscribe(function(msg) {
+    baro = msg.altitude;
+  });
+
+  var mag_listener = new ROSLIB.Topic({
+    ros: ros,
+    name: '/magnetometer',
+    messageType: 'sensor_msgs/MagneticField'
+  });
+
+  mag_listener.subscribe(function(msg) {
+    mag_x = msg.magnetic_field.x;
+    mag_y = msg.magnetic_field.y;
+    mag_z = msg.magnetic_field.z;
   });
 }
 
 var tv = 1/60 * 1000;
+var plot_height = 200;
+var plot_width = 800;
+var num_data_points = 180;
 
-// instantiate our graph!
-var graph = new Rickshaw.Graph({
+
+//-------------------------------------------------
+// ACCELEROMETER
+var acc_graph = new Rickshaw.Graph({
   element: document.getElementById("acc_chart"),
-  width: 1200,
-  height: 400,
+  width: plot_width,
+  height: plot_height,
   renderer: 'line',
   min: 'auto',
   series: new Rickshaw.Series.FixedDuration([{
@@ -53,21 +81,21 @@ var graph = new Rickshaw.Graph({
     name: 'acc_z'
   }], undefined, {
     timeInterval: tv,
-    maxDataPoints: 180,
+    maxDataPoints: num_data_points,
     timeBase: new Date().getTime() / 1000
   })
 
 });
-graph.render();
+acc_graph.render();
 
 var xAxis = new Rickshaw.Graph.Axis.Time({
-    graph: graph,
+    graph: acc_graph,
     timeFixture: new Rickshaw.Fixtures.Time.Local()
 });
 xAxis.render();
 
 var y_ticks = new Rickshaw.Graph.Axis.Y( {
-  graph: graph, 
+  graph: acc_graph, 
   orientation: 'left',
   tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
   element: document.getElementById('acc_y_axis')
@@ -86,17 +114,144 @@ function calibrate_imu() {
 };
 
 
-// add some data every so often
+//-------------------------------------------------
+// GYRO
+var gyro_graph = new Rickshaw.Graph({
+  element: document.getElementById("gyro_chart"),
+  width: plot_width,
+  height: plot_height,
+  renderer: 'line',
+  min: 'auto',
+  series: new Rickshaw.Series.FixedDuration([{
+    name: 'gyro_x'
+  }, {
+    name: 'gyro_y'
+  }, {
+    name: 'gyro_z'
+  }], undefined, {
+    timeInterval: tv,
+    maxDataPoints: num_data_points,
+    timeBase: new Date().getTime() / 1000
+  })
+
+});
+gyro_graph.render();
+
+var xAxis = new Rickshaw.Graph.Axis.Time({
+    graph: gyro_graph,
+    timeFixture: new Rickshaw.Fixtures.Time.Local()
+});
+xAxis.render();
+
+var y_ticks = new Rickshaw.Graph.Axis.Y( {
+  graph: gyro_graph, 
+  orientation: 'left',
+  tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+  element: document.getElementById('gyro_y_axis')
+} );
+
+//-------------------------------------------------
+// Baro
+var baro_graph = new Rickshaw.Graph({
+  element: document.getElementById("baro_chart"),
+  width: plot_width,
+  height: plot_height,
+  renderer: 'line',
+  min: 'auto',
+  series: new Rickshaw.Series.FixedDuration([{
+    name: 'baro' }], undefined, {
+    timeInterval: tv,
+    maxDataPoints: num_data_points,
+    timeBase: new Date().getTime() / 1000
+  })
+
+});
+baro_graph.render();
+
+var xAxis = new Rickshaw.Graph.Axis.Time({
+    graph: baro_graph,
+    timeFixture: new Rickshaw.Fixtures.Time.Local()
+});
+xAxis.render();
+
+var y_ticks = new Rickshaw.Graph.Axis.Y( {
+  graph: baro_graph, 
+  orientation: 'left',
+  tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+  element: document.getElementById('baro_y_axis')
+} );
+
+//-------------------------------------------------
+// Magnetometer
+var mag_graph = new Rickshaw.Graph({
+  element: document.getElementById("mag_chart"),
+  width: plot_width,
+  height: plot_height,
+  renderer: 'line',
+  min: 'auto',
+  series: new Rickshaw.Series.FixedDuration([{
+    name: 'mag_x'
+  }, {
+    name: 'mag_y'
+  }, {
+    name: 'mag_z'
+  }], undefined, {
+    timeInterval: tv,
+    maxDataPoints: num_data_points,
+    timeBase: new Date().getTime() / 1000
+  })
+
+});
+mag_graph.render();
+
+var xAxis = new Rickshaw.Graph.Axis.Time({
+    graph: baro_graph,
+    timeFixture: new Rickshaw.Fixtures.Time.Local()
+});
+xAxis.render();
+
+var y_ticks = new Rickshaw.Graph.Axis.Y( {
+  graph: baro_graph, 
+  orientation: 'left',
+  tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+  element: document.getElementById('mag_y_axis')
+} );
+
+
+
+
+//-------------------------------------------------
+// Render Update Loop
 var i = 0;
 var iv = setInterval(function() {
 
-  var data = {
+  var acc_data = {
     one: acc_x
   };
-  data.two = acc_y
-  data.three = acc_z;
+  acc_data.two = acc_y
+  acc_data.three = acc_z;
+  acc_graph.series.addData(acc_data);
+  acc_graph.update();
 
-  graph.series.addData(data);
-  graph.update();
+  var gyro_data = {
+    one: gyro_x
+  };
+  gyro_data.two = gyro_y
+  gyro_data.three = gyro_z;
+  gyro_graph.series.addData(gyro_data);
+  gyro_graph.update();
 
+  var baro_data = { 
+    one: baro 
+  };
+  baro_graph.series.addData(baro_data);
+  baro_graph.update();
+
+  var mag_data = {
+    one: mag_x
+  };
+  mag_data.two = mag_y
+  mag_data.three = mag_z;
+  mag_graph.series.addData(mag_data);
+  mag_graph.update();
 }, tv);
